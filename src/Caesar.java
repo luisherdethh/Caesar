4697c5b60bff899decb69d1cff4231b7ab078f2a
Luis Linux <luis.herdeth@gmail.com>
2016-11-30 21:22:09 +0100

    first commit

---
+++ src/Caesar.java
@@ -0,0 +1,31 @@
+
+public class Caesar {
+    static String cypher(String s, int delta) {
+        StringBuilder cifrado = new StringBuilder(s.length());
+        int caracter;
+        for (int i=0; i<s.length(); i++){
+            caracter = (int) s.charAt(i); //Devuelve el carácter primera posición 0.
+            if((caracter>=65) && (caracter<=122)){
+                caracter -= 32; //vuelve al caracter 90 (z)
+            }
+            if((caracter>=65) && (caracter<=90)){
+                caracter += delta;
+                while (caracter>90){
+                    caracter -= 90;
+                    caracter += 64;
+                }
+            }
+            cifrado.append((char) caracter); //caracter de Caesar
+        }
+        s = cifrado.toString(); //Devuelve una cadena de texto (String)
+        return s.toUpperCase(); //Pasamos a mayúsculas
+    }
+
+    static String decypher(String s, int delta) {
+        return null;
+    }
+
+    static String magic(String s) {
+        return null;
+    }
+}
